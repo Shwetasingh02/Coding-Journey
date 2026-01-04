6f606cf2e9843056a441d577f0e43a22fd429e6d
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-01-04 23:25:09 +0530

    Daily DSA Problem

---
+++ BitwiseOperator/leetcode/fourDivisor1390.java
@@ -0,0 +1,26 @@
+package BitwiseOperator.leetcode;
+
+public class fourDivisor1390 {
+
+    public static void main(String[] args) {
+
+        int [] nums={21,4,7};
+        int ans=sumFourDivisors(nums);
+        System.out.println(ans);
+    }
+    public static int sumFourDivisors(int[] nums) {
+
+        int count=0, sum=0;
+        int i=1;
+
+        for(int n:nums){
+            while(i<= n){
+                if(n%i==0)
+                    count++;
+
+                i++;
+            }
+        }
+        return count;
+    }
+}
