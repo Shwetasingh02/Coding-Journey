b5239e2c2126814c41c7e1de34dd54def2221519
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-01-19 09:58:51 +0530

    String Coding + Theory

---
+++ stringHub/Introduction.java
@@ -0,0 +1,15 @@
+package stringHub;
+
+public class Introduction {
+    public static void main(String[] args) {
+
+        // this isss the way to define the string in the java
+//        String name="Shweta";
+//        System.out.println(name);
+//
+        String a="Shweta";
+        System.out.println(a);
+        a="Singh";
+        System.out.println(a);
+    }
+}
