c45b81aa99eb3d457a5f9dc5783053694964567b
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-03-10 23:27:29 +0530

    Daily DSA

--- Recursion/DigitSum.java
+++ Recursion/DigitSum.java
@@ -1,2 +1,15 @@
-package Recursion;public class DigitSum {
+package Recursion;
+
+public class DigitSum {
+
+    public static void main(String[] args) {
+        int ans = sumOf(1342);
+        System.out.println(ans);
+    }
+    static int sumOf(int n){
+        if(n<=0){
+            return 0;
+        }
+        return (n%10) + sumOf(n/10);
+    }
 }
