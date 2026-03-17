c7a2a9adeddbe90ff52c5c4cf401842ae063fa7c
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-03-17 23:12:57 +0530

    daily DSA

---
+++ BitwiseAndMath/leetcode/PowofN50.java
@@ -0,0 +1,11 @@
+package BitwiseAndMath.leetcode;
+
+public class PowofN50 {
+
+    public static void main(String[] args) {
+
+        int x=2, n=10;
+
+        System.out.println(Math.pow(2,10));
+    }
+}
