9d5f96d23b06649489dc9508bffe40c36b3d727b
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-03-21 22:01:00 +0530

    git

--- stringHub/LeetcodeQuestions/ReverseString.java
+++ stringHub/LeetcodeQuestions/ReverseString.java
@@ -5,8 +5,11 @@ public class ReverseString {
     public static void main(String[] args) {
         char[] s = {'h','e','l','l','o'};
 
+        // with loop
         for(int i=s.length/-1 ;i>=0;i--){
          System.out.print(s[i] + " ");
         }
+
+        // without loop
     }
 }
