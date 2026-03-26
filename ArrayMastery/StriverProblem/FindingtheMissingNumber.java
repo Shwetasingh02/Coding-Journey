b23f303e996d3f5db49a98eb7114cc3c01645c39
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-03-26 23:33:36 +0530

    Find Missing Number

---
+++ ArrayMastery/StriverProblem/FindingtheMissingNumber.java
@@ -0,0 +1,19 @@
+package ArrayMastery.StriverProblem;
+
+public class FindingtheMissingNumber {
+
+    public static void main(String[] args) {
+
+        int [] arr ={1,2,4,5};
+        int n = arr.length+1;
+        int expectedSum = n*(n-1)/2;
+
+        int sum=0;
+        for(int j : arr){
+            sum=+j;
+        }
+
+
+        System.out.print(expectedSum-sum);
+    }
+}
