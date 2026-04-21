8cc8d22ecf0be9ba6d3a8629e9814df324affb6e
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-04-22 00:02:21 +0530

    daily dsa

--- PatternQuestions/Practice.java
+++ PatternQuestions/Practice.java
@@ -13,7 +13,7 @@ public class Practice {
 
             // print stars
             for(int j = 1; j <= (2*i - 1); j++) {
-                System.out.print("*");
+                System.out.print("**");
             }
 
             System.out.println();
