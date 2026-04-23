a88a5fb217d3f770ae2ee471ef73bfd7be52658e
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-04-23 22:42:39 +0530

    daily DSA

--- ArrayMastery/StriverProblem/UniqueNumber.java
+++ ArrayMastery/StriverProblem/UniqueNumber.java
@@ -4,12 +4,17 @@ public class UniqueNumber {
 
     public static void main(String[] args) {
 
-        int arr [] = {1,2,2,4,4,1,5};
+        int arr [] = {1,2,3,4};
         int result =0;
         for(int j : arr){
              result = result ^ j;
-
         }
-        System.out.print(result);
+        System.out.println(result);
+       if(result >0){
+           System.out.println("true");
+       }
+       else{
+           System.out.println("False");
+       }
     }
 }
