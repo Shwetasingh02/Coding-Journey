486fe2063d4b4f6115b72ab2f66db779369f1ce6
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-04-26 21:13:35 +0530

    Daily DSA

--- PatternQuestions/Practice.java
+++ PatternQuestions/Practice.java
@@ -3,21 +3,23 @@ package PatternQuestions;
 public class Practice {
     public static void main(String[] args) {
 
-        int n=5;
-        for(int i = 1; i <= n; i++) {
+        int n = 5;
 
-            // print spaces
-            for(int j = 1; j <= n - i; j++) {
+        char[] arr = {'a', 'b', 'c', 'd'};  // tum yaha size change kar sakti ho
+
+        for (int i = 1; i <= n; i++) {
+
+            // print stars
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print("*");
             }
 
-            // print stars
-            for(int j = 1; j <= (2*i - 1); j++) {
-                System.out.print("a");
+            // print characters
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(arr[(j - 1) % arr.length]);
             }
 
             System.out.println();
         }
-
     }
 }
