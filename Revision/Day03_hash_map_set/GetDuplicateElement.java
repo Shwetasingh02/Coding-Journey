c2fe0d6167a0de5272d164c7ccb7957da5b08510
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-09-26 11:40:48 +0530

    Duplicate Number

---
+++ Revision/Day03_hash_map_set/GetDuplicateElement.java
@@ -0,0 +1,20 @@
+package Revision.Day03_hash_map_set;
+
+import java.util.HashSet;
+
+public class GetDuplicateElement {
+
+    public static void main(String [] arg){
+        HashSet<Integer> number =new HashSet<>();
+        int[] arr = {10, 20, 30, 20, 40, 10, 50};
+
+        for(int num : arr){
+            if(number.contains(num)){
+                System.out.println("First Duplicate : " + num);
+                break;
+            }else{
+                number.add(num);
+            }
+        }
+    }
+}
