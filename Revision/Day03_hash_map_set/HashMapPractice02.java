f7dce1ca6093a55c0beb636c14f0c1aeb1b1d74b
Shweta Singh <singhshwetayadav1999@gmail.com>
2026-09-26 11:26:15 +0530

    HashSet Practice

--- Revision/Day03_hash_map_set/HashMapPractice02.java
+++ Revision/Day03_hash_map_set/HashMapPractice02.java
@@ -1,10 +1,11 @@
 package Revision.Day03_hash_map_set;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class HashMapPractice02 {
 
-    public static void main(String [] arg){
+    public static <arr> void main(String [] arg){
 
         // key set
         HashMap<String, Integer> marks = new HashMap<>();
@@ -17,10 +18,35 @@ public class HashMapPractice02 {
 
         // Har student ka naam alag-alag line mein print karo.
 
-        for(String key : marks.keySet()){
-            System.out.println(key +" : " + marks.get(key));
+//        for(String key : marks.keySet()){
+//            System.out.println(key +" : " + marks.get(key));
+//
+//        }
 
+        // Print Sirf un students ko print karo jinke marks 90 ya usse zyada hain.
+
+//        for(String key : marks.keySet() ){
+//            if(marks.get(key) >=90){
+//                System.out.println(key +" : " + marks.get(key));
+//            }
+//        }
+
+
+        // Array ke unique elements print karo using HashSet.
+
+        HashSet<Integer> num = new HashSet<>();
+        int[] arr = {10, 20, 10, 30, 20, 40, 30, 50};
+
+        for(Integer value : arr){
+        num.add(value);
         }
+        System.out.print(num);
 
+//        for(int number: arr){
+//            if(num.contains(number)){
+//                System.out.print("Duplicate");
+//            }
+//            num.add(number);
+//        }
     }
 }
